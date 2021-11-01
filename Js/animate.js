e9e0206024aa9c7a140e5184e9268e8b8b421d04
Killian <brunetkillian@yahoo.fr>
2021-11-01 19:06:04 +0800

    Js et animation update

---
+++ Js/animate.js
@@ -0,0 +1,6 @@
+if (window.sessionStorage.getItem('animated') === null) {
+    document.getElementById("animationLogo").classList.remove("d-none");
+    document.getElementById("animationLogo").classList.add("animationLogo");
+    document.getElementById("logo").classList.add("logoAnim");
+    window.sessionStorage.setItem('animated' ,1);
+}
